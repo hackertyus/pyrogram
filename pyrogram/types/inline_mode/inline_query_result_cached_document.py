5d23868c66ef37e2fa304b32797216d09abc2d4a
hackertyus <88194972+hackertyus@users.noreply.github.com>
2022-03-21 00:18:51 +0300

    Update inline_query_result_cached_document.py

--- pyrogram/types/inline_mode/inline_query_result_cached_document.py
+++ pyrogram/types/inline_mode/inline_query_result_cached_document.py
@@ -1,5 +1,5 @@
 #  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-2020 Dan <https://github.com/delivrance>
+#  Copyright (C) 2017-2021 Dan <https://github.com/delivrance>
 #
 #  This file is part of Pyrogram.
 #
@@ -16,35 +16,35 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Union
+from typing import Optional, List
 
-from pyrogram import raw
-from pyrogram import utils
-from pyrogram import types
-from pyrogram.parser import Parser
+import pyrogram
+from pyrogram import raw, types, utils
 from .inline_query_result import InlineQueryResult
 
 
 class InlineQueryResultCachedDocument(InlineQueryResult):
     """Link to a file stored on the Telegram servers.
+    
     By default, this file will be sent by the user with an optional caption. 
     Alternatively, you can use input_message_content to send a message with the specified content instead of the file.
+    
     Parameters:
         title (``str``):
             Title for the result.
         
         file_id (``str``):
             Pass a file_id as string to send a media that exists on the Telegram servers.
-        file_ref (``str``, *optional*):
-            A valid file reference obtained by a recently fetched media message.
-            To be used in combination with a file id in case a file reference is needed.
+            
         id (``str``, *optional*):
             Unique identifier for this result, 1-64 bytes.
             Defaults to a randomly generated UUID4.
+            
         description (``str``, *optional*):
             Short description of the result.
+            
         caption (``str``, *optional*):
-            Caption of the photo to be sent, 0-1024 characters.
+            Caption of the document to be sent, 0-1024 characters.
         
         parse_mode (``str``, *optional*):
             By default, texts are parsed using both Markdown and HTML styles.
@@ -52,8 +52,12 @@ class InlineQueryResultCachedDocument(InlineQueryResult):
             Pass "markdown" or "md" to enable Markdown-style parsing only.
             Pass "html" to enable HTML-style parsing only.
             Pass None to completely disable style parsing.
+        caption_entities (List of :obj:`~pyrogram.types.MessageEntity`):
+                List of special entities that appear in the caption, which can be specified instead of *parse_mode*.
+            
         reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup`, *optional*):
             Inline keyboard attached to the message.
+            
         input_message_content (:obj:`~pyrogram.types.InputMessageContent`):
             Content of the message to be sent.
     """
@@ -62,27 +66,31 @@ class InlineQueryResultCachedDocument(InlineQueryResult):
         self,
         title: str,
         file_id: str,
-        file_ref: str = None,
         id: str = None,
         description: str = None,
         caption: str = "",
-        parse_mode: Union[str, None] = object,
+        parse_mode: Optional[str] = object,
+        caption_entities: List["types.MessageEntity"] = None,
         reply_markup: "types.InlineKeyboardMarkup" = None,
         input_message_content: "types.InputMessageContent" = None
     ):
         super().__init__("file", id, input_message_content, reply_markup)
 
         self.file_id = file_id
-        self.file_ref = file_ref
         self.title = title
         self.description = description
         self.caption = caption
+        self.caption_entities = caption_entities
         self.parse_mode = parse_mode
         self.reply_markup = reply_markup
         self.input_message_content = input_message_content
 
-    async def write(self):
+    async def write(self, client: "pyrogram.Client"):
         document = utils.get_input_file_from_file_id(self.file_id)
+
+        message, entities = (await utils.parse_text_entities(
+            client, self.caption, self.parse_mode, self.caption_entities
+        )).values()
 
         return raw.types.InputBotInlineResultDocument(
             id=self.id,
@@ -91,11 +99,12 @@ class InlineQueryResultCachedDocument(InlineQueryResult):
             description=self.description,
             document=document,
             send_message=(
-                await self.input_message_content.write(self.reply_markup)
+                await self.input_message_content.write(client, self.reply_markup)
                 if self.input_message_content
                 else raw.types.InputBotInlineMessageMediaAuto(
-                    reply_markup=self.reply_markup.write() if self.reply_markup else None,
-                    **await (Parser(None)).parse(self.caption, self.parse_mode)
+                    reply_markup=await self.reply_markup.write(client) if self.reply_markup else None,
+                    message=message,
+                    entities=entities
                 )
             )
         )
