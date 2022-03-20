24c9dedd85a84b25f06491ad7d12c96dfc57c301
hackertyus <88194972+hackertyus@users.noreply.github.com>
2022-03-21 00:53:46 +0300

    Update utils.py

--- pyrogram/utils.py
+++ pyrogram/utils.py
@@ -38,7 +38,43 @@ async def ainput(prompt: str = "", *, hide: bool = False):
         func = functools.partial(getpass if hide else input, prompt)
         return await asyncio.get_event_loop().run_in_executor(executor, func)
 
+def get_input_file_from_file_id(
+    file_id: str,
+    expected_file_type: FileType = None,
+) -> Union["raw.types.InputPhoto", "raw.types.InputDocument"]:
+    try:
+        decoded = FileId.decode(file_id)
+    except Exception:
+        raise ValueError(
+            f'Failed to decode "{file_id}". The value does not represent an existing local file, '
+            f'HTTP URL, or valid file id.'
+        )
 
+    file_type = decoded.file_type
+
+    if expected_file_type is not None and file_type != expected_file_type:
+        raise ValueError(f'Expected: "{expected_file_type}", got "{file_type}" file_id instead')
+
+    if file_type in (FileType.THUMBNAIL, FileType.CHAT_PHOTO):
+        raise ValueError(f"This file_id can only be used for download: {file_id}")
+
+    if file_type in PHOTO_TYPES:
+        return raw.types.InputPhoto(
+            id=decoded.media_id,
+            access_hash=decoded.access_hash,
+            file_reference=decoded.file_reference,
+        )
+
+    if file_type in DOCUMENT_TYPES:
+        return raw.types.InputDocument(
+            id=decoded.media_id,
+            access_hash=decoded.access_hash,
+            file_reference=decoded.file_reference,
+        )
+
+    raise ValueError(f"Unknown file id: {file_id}")
+    
+    
 def get_input_media_from_file_id(
     file_id: str,
     expected_file_type: FileType = None
